459839dfbfd0fbf71f5b2779fa680e1f6c255bed
aishwaryaps2002 <133131171+aishwaryaps2002@users.noreply.github.com>
2025-09-15 21:48:10 +0530

    Mileage remuneration.cpp

--- Program 1/program1.cpp
+++ Program 1/program1.cpp
@@ -1,5 +1,20 @@
 #include <iostream>
+#include <iomanip>
 using namespace std;
 int main()
 {
-  cout <<"MILEAGE REMUNERATION 
+	float n,d,b;
+	int r;
+	cout<<"Enter beginning odometer reading:";
+	cin>>n;
+	cout<<"Enter ending odometer reading:";
+	cin>>d;
+	b=d-n;
+	r=b*25;
+	cout<<fixed<<setprecision(1);
+	cout<<"You travelled " <<b <<" miles.";
+	cout<<"At Rs.25 per mile,your remuneration is Rs."<<r;
+	return 0;
+}
+	
+
